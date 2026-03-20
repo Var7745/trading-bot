23a8a2e95efb5ab28bb5889d74408ee04c5a15e6
Var7745 <goudvarshith35@gmail.com>
2026-03-20 15:21:26 +0530

    Refactor _application variable declaration
    Removed global declaration for _application.

--- bot.py
+++ bot.py
@@ -1495,7 +1495,6 @@ if __name__ == "__main__":
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
 
     # Store reference for background threads
-    global _application
     _application = app
 
     # Start background threads
